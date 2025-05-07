82c1d5c56f0f5c239310cf44107a91d12ff13691
XieZongChen <952937570@qq.com>
2025-05-07 11:12:20 +0800

    docs: init three.js

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue';
+import { init } from './init';
+
+const msg = ref('');
+const jumpRef = ref<((color: string) => void) | null>(null);
+
+const setMsg = (message: string) => {
+  msg.value = message;
+};
+
+const jumpBox = (color: string) => {
+  if (jumpRef.value) {
+    jumpRef.value(color);
+  }
+};
+
+onMounted(() => {
+  const dom = document.querySelector('#content');
+  if (!dom) return;
+  const { jump } = init(dom, setMsg);
+  jumpRef.value = jump;
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div id="header">vue & three.js</div>
+    <div id="main">
+      <div id="content"></div>
+      <div id="operate">
+        <button @click="jumpBox('red')">red</button>
+        <button @click="jumpBox('green')">green</button>
+        <button @click="jumpBox('blue')">blue</button>
+        <p>{{ msg }}</p>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
