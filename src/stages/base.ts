9e387b81e39339f09c8e148bd1b7b61217d6a533
XieZongChen <952937570@qq.com>
2025-05-13 11:36:06 +0800

    feat: testStage 开发

--- src/stages/base.ts
+++ src/stages/base.ts
@@ -1,3 +1,20 @@
+import * as THREE from 'three';
+import { Group } from '@tweenjs/tween.js';
+
+export interface IStageBaseOptions {
+  scene: THREE.Scene; // 当前显示的场景
+  camera: THREE.PerspectiveCamera; // 摄像机
+  tweenGroup: Group; // 动画组
+}
+
 export abstract class BaseStage {
-    abstract clear(): void;
+  scene: THREE.Scene; // 当前显示的场景
+  camera: THREE.PerspectiveCamera; // 摄像机
+  tweenGroup: Group; // 动画组
+  constructor({ scene, camera, tweenGroup }: IStageBaseOptions) {
+    this.scene = scene;
+    this.camera = camera;
+    this.tweenGroup = tweenGroup;
+  }
+  abstract clear(): void;
 }
---
+++ src/stages/testStage.ts
@@ -0,0 +1,48 @@
+import * as THREE from 'three';
+import { BaseStage, type IStageBaseOptions } from './base';
+
+export class TestStage extends BaseStage {
+  axesHelper: THREE.AxesHelper;
+  directionalLight: THREE.DirectionalLight;
+  ambientLight: THREE.AmbientLight;
+  mesh: THREE.Group;
+  constructor(options: IStageBaseOptions) {
+    super(options);
+    this.axesHelper = new THREE.AxesHelper(500);
+    this.scene.add(this.axesHelper);
+
+    this.directionalLight = new THREE.DirectionalLight(0xffffff);
+    this.directionalLight.position.set(500, 400, 300);
+    this.scene.add(this.directionalLight);
+
+    this.ambientLight = new THREE.AmbientLight(0xffffff);
+    this.scene.add(this.ambientLight);
+
+    this.mesh = new THREE.Group();
+    this.initMesh();
+  }
+
+  private initMesh() {
+    this.mesh.add(this.createBox('red', 0));
+    this.mesh.add(this.createBox('blue', 300));
+    this.mesh.add(this.createBox('green', -300));
+  }
+
+  private createBox(color: string, x: number) {
+    const geometry = new THREE.BoxGeometry(100, 100, 100);
+    const material = new THREE.MeshPhongMaterial({
+      color: color,
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = x;
+    mesh.name = color;
+    return mesh;
+  }
+
+  clear() {
+    this.scene.remove(this.axesHelper);
+    this.scene.remove(this.directionalLight);
+    this.scene.remove(this.ambientLight);
+    this.scene.remove(this.mesh);
+  }
+}
