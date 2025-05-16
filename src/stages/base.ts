66f0d13bc39e9cc6125e078065c5c1f18950464d
XieZongChen <952937570@qq.com>
2025-05-16 17:11:26 +0800

    feat: graphical 增加全局 GUI 控件

--- src/stages/base.ts
+++ src/stages/base.ts
@@ -13,10 +13,12 @@ export abstract class BaseStage {
   scene: THREE.Scene; // 当前显示的场景
   camera: THREE.PerspectiveCamera; // 摄像机
   tweenGroup: Group; // 动画组
-  constructor({ scene, camera, tweenGroup }: IStageBaseOptions) {
+  GUI: GUI; // GUI 控件
+  constructor({ scene, camera, tweenGroup, GUI }: IStageBaseOptions) {
     this.scene = scene;
     this.camera = camera;
     this.tweenGroup = tweenGroup;
+    this.GUI = GUI;
   }
   abstract clear(): void;
 }
